698700a2dc8e05b6a3674baaa16b8f7179b7b96b
Srushti-9 <srushtisuvarna77@gmail.com>
2024-04-08 17:19:17 +0530

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { BrowserRouter, Routes, Route, Link  } from "react-router-dom";
+import Details from "./Details";
+
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(SearchParams),
-    React.createElement(SearchParams),
-    React.createElement(SearchParams),
-  ]);
+  return(
+    <div>
+      <BrowserRouter>
+      <header>
+  <Link to="/">Adopt Me!</Link>
+</header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+  )
 };
 
 const container = document.getElementById("root");
