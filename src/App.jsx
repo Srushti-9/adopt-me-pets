664e43851d298100c57e964f3ef288953e28880e
Srushti-9 <srushtisuvarna77@gmail.com>
2024-04-08 17:46:53 +0530

    Add caching using react-query

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import { BrowserRouter, Routes, Route, Link  } from "react-router-dom";
 import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return(
     <div>
       <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+
       <header>
   <Link to="/">Adopt Me!</Link>
 </header>
@@ -16,6 +27,8 @@ const App = () => {
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
       </Routes>
+      </QueryClientProvider>
+
     </BrowserRouter>
     </div>
   )
